9604cb6be85871436da17a2366477968749480b1
Roger Pérez Blanco <rogerpb98@gmail.com>
2021-12-20 01:39:47 +0100

    categories WIP

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import requests
 import datetime
 import json
 
+def sanitizeString(string):
+    string = string.strip()
+    string = string.replace("<b>", '')
+    string = string.replace("</b>", '')
+    return string
+
 def extractBonus(document):
     #Find the div where the almanax bonus is
     bonusDiv = document.find_all("div", {"class": "more"})
@@ -15,25 +21,20 @@ def extractBonus(document):
         if str(i).startswith("<div"): 
             break
         almanaxBonus += str(i)
-    #Sanitize string
-    almanaxBonus = almanaxBonus.strip()
-    almanaxBonus = almanaxBonus.replace("<b>", '')
-    almanaxBonus = almanaxBonus.replace("</b>", '')
+
+    almanaxBonus = sanitizeString(almanaxBonus)
     return almanaxBonus
 
 def extractQuest(document):
-    #Find the div where the almanax bonus is
+    #Find the div where the almanax quest is
     questDiv = document.find("div", {"class": "more-infos-content"})
     #Extract the contents into an array because its a fucking mess
     elements = questDiv.contents
     children = list(elements[1].descendants)
     almanaxQuest = str(children[2]) #this is where the quest itself is
 
-    #Sanitize string
-    almanaxQuest = almanaxQuest.strip()
-    almanaxQuest = almanaxQuest.replace("<b>", '')
-    almanaxQuest = almanaxQuest.replace("</b>", '')
-    
+    almanaxQuest = sanitizeString(almanaxQuest)
+    print(almanaxQuest)
     return almanaxQuest
 
 def extractItem(string):
@@ -43,11 +44,18 @@ def extractItem(string):
     string = string.split()
     return string
 
-def logOutput(array):
-    f = open("log.txt", "w") # "w" = overwrite, "a" = append
-    for i in array:
-        f.write(i)
-    f.close()
+def defineCategory(string):
+    if string.find("more quickly") < 0:
+        return "Ressource respawn rate"
+    if string.find("chances") < 0:
+        return "Drop rate"
+    if string.find("save") < 0 or string.find("chance") < 0:
+        return "Crafting save"
+    return ""
+
+def overwriteJsonData(data):
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
 
 def main():
     start_date = datetime.date(2021, 1, 1)
@@ -60,13 +68,13 @@ def main():
 
     while start_date <= end_date: #Iterate through all dates
         url = "http://www.krosmoz.com/en/almanax/"+str(start_date)+""
-
         result = requests.get(url)
         doc = BeautifulSoup(result.text, "html.parser")
 
         bonus = extractBonus(doc)
         quest = extractQuest(doc)
         item = extractItem(quest)
+        #category = defineCategory(bonus)
 
         data['Almanax'].append({
             'date': str(start_date),
@@ -76,12 +84,12 @@ def main():
             'item': {
                 'quantity': item[0],
                 'name': " ".join(item[1:]) #Concatenates every word of the item name
-            }
+            }#,
+            #'category' : category
         })
         start_date += delta
-
-    with open('data.json', 'w') as outfile:
-        json.dump(data, outfile)
+    
+    overwriteJsonData(data)
 
 if __name__ == "__main__":
     main()
